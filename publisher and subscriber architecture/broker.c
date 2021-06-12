ce10f01654ae04a64332bff050ac8f5936d66f3f
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-06-12 22:08:31 +0900

    renamed dirs / pub-sub middle-ware initial commit

---
+++ publisher and subscriber architecture/broker.c
@@ -0,0 +1,25 @@
+#define _POSIX_SOURCE
+#include <unistd.h>
+#include <dirent.h>
+#include <errno.h>
+#include <pthread.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#undef _POSIX_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+#define SUBSCRIBER_PORT (1000)
+
+int main(int argc, const char *argv[])
+{
+}
---
+++ publisher and subscriber architecture/subscriber.c
@@ -0,0 +1,23 @@
+#define _POSIX_SOURCE
+#include <unistd.h>
+#include <dirent.h>
+#include <errno.h>
+#include <pthread.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#undef _POSIX_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, const char *argv[])
+{
+}
